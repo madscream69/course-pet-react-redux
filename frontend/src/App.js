18b0dfc96a65a71e56c946612f195c10e1a128ef
Dolbezhnik <krendelalenkin@gmail.com>
2024-09-16 21:39:41 +0300

    start point

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>App</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
